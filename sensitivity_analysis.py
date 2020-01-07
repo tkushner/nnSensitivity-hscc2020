1713c0bc63e32d29dd383e79893a44545d6f37a9
tkushner <Taisa.Kushner@Colorado.EDU>
2020-01-07 15:15:12 -0700

    range output plots

--- sensitivity_analysis.py
+++ sensitivity_analysis.py
@@ -25,30 +25,36 @@ for r, d, f in os.walk(path):
             else:
                 print('improper file name found: '+str(os.path.join(r,file)))
 
+reg_data = []
 reg_sens_low = []
 reg_sens_high = []
 for file in reg_files:
     _reg_data = pd.read_csv(file,header=None)
     _lows = _reg_data.iloc[:,0:19:2]
     _highs = _reg_data.iloc[:,1:20:2]
+    reg_data.append(_reg_data)
     reg_sens_low.append(_lows.diff(axis=1).dropna(axis=1))
     reg_sens_high.append(_highs.diff(axis=1).dropna(axis=1))
 
+split_data = []
 split_sens_low = []
 split_sens_high = []
 for file in split_files:
     _split_data = pd.read_csv(file,header=None)
     _lows = _split_data.iloc[:,0:19:2]
     _highs = _split_data.iloc[:,1:20:2]
+    split_data.append(_split_data)
     split_sens_low.append(_lows.diff(axis=1).dropna(axis=1))
     split_sens_high.append(_highs.diff(axis=1).dropna(axis=1))
 
+cons_data = []
 cons_sens_low = []
 cons_sens_high = []
 for file in cons_files:
     _cons_data = pd.read_csv(file,header=None)
     _lows = _cons_data.iloc[:,0:19:2]
     _highs = _cons_data.iloc[:,1:20:2]
+    cons_data.append(_cons_data)
     cons_sens_low.append(_lows.diff(axis=1).dropna(axis=1))
     cons_sens_high.append(_highs.diff(axis=1).dropna(axis=1))
 
@@ -63,6 +69,25 @@ split_frame = pd.concat([split_low_big,split_high_big],axis=1)
 cons_low_big = pd.concat(cons_sens_low,axis=1)
 cons_high_big = pd.concat(cons_sens_high,axis=1)
 cons_frame = pd.concat([cons_low_big,cons_high_big],axis=1)
+
+def PlotRanges(df_lst, netname, IDX):
+    #get and plot the means for a specific index location
+    df = df_lst[IDX]
+    df_l = df.iloc[:,0:20:2]
+    df_h = df.iloc[:,1:20:2]
+
+    fig = plt.figure(dpi=300)
+    #subplot each input location
+    for idx in df_l.index:
+        lows = df_l.iloc[idx,:].reset_index(drop=True)
+        highs = df_h.iloc[idx,:].reset_index(drop=True)
+        df_sub = pd.concat([lows,highs],axis=1)
+        df_sub.columns = ['low','high']
+        ax_num = fig.add_subplot(240+idx+1)
+        df_sub.plot(ax=ax_num,marker='o')
+        ax_num.set_xlim([0,10])
+
+    plt.savefig(netname+'_'+str(IDX)+'_ranges_by_location.pdf')
 
 
 def PlotSensitivity(df, netname, printVal):
@@ -82,8 +107,7 @@ def PlotSensitivity(df, netname, printVal):
         print('--std--')
         print(df_pos.std(axis=1,skipna=True))
 
-    #plot means for positive and negative
-    #make dataframe of means
+    #plot means for positive and negative sensitivities, save as pdf
     df_mean = pd.concat([df_neg.mean(axis=1,skipna=True),df_pos.mean(axis=1,skipna=True)],axis=1)
     df_mean.columns=['Negative','Positive']
     df_mean.plot(kind='bar',stacked=True)
@@ -100,3 +124,8 @@ def PlotSensitivity(df, netname, printVal):
 PlotSensitivity(reg_frame,'M1-Regular',False)
 PlotSensitivity(split_frame,'M2-Split',False)
 PlotSensitivity(cons_frame,'M3-Constrained',False)
+
+#create a pdf plot for the random selected ranges by location plots
+PlotRanges(reg_data,'M1-Regular',8)
+PlotRanges(split_data,'M2-Split',8)
+PlotRanges(cons_data,'M3-Constrained',8)
